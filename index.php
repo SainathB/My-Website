6f1d1a25fe705970d4e40d70a5f6d72eed375ea9
SainathB <sainathbatthala@gmail.com>
2016-02-19 11:38:56 +0530

    index.html renamed to index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title >
+    	Sainath Batthala
+    </title>
+   
+       
+        
+    
+    <link rel="stylesheet" type="text/css" href="index.css">
+    <link rel="icon" type="image/jpg" href="logo.jpg">
+
+        
+    </head>
+    <body class="background">
+    
+    </body>
+      
+
+</html>
